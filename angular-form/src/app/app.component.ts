d13f55ca7a5037450f46c9a1652db85d2b478ed0
MZ <nikita.zholkovskyi@gmail.com>
2020-12-07 14:48:41 +0100

    step 12 finished

--- angular-form/src/app/app.component.ts
+++ angular-form/src/app/app.component.ts
@@ -11,5 +11,11 @@ export class AppComponent {
   title = 'angular-form';
   languages = [{name: 'JavaScript'}, {name: 'PHP'}, {name: 'English'}];
   friendModel = new Friend(null,null,null,null, null)
+
+  // constructor() { }
+
+  onSubmit(form) {
+    console.log(form.value)
+  }
 }
 
--- angular-form/src/app/friend.ts
+++ angular-form/src/app/friend.ts
@@ -13,4 +13,43 @@ export class Friend {
     this._favLang = favLang;
   }
 
+  get firstName(): string {
+    return this._firstName;
+  }
+
+  get lastName(): string {
+    return this._lastName;
+  }
+
+  get email(): string {
+    return this._email;
+  }
+
+  get phone(): string {
+    return this._phone;
+  }
+
+  get favLang(): string {
+    return this._favLang;
+  }
+
+  set firstName(value: string) {
+    this._firstName = value;
+  }
+
+  set lastName(value: string) {
+    this._lastName = value;
+  }
+
+  set email(value: string) {
+    this._email = value;
+  }
+
+  set phone(value: string) {
+    this._phone = value;
+  }
+
+  set favLang(value: string) {
+    this._favLang = value;
+  }
 }
